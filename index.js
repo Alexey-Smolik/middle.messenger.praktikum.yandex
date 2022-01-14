88a1d7b998d4b4cc649c0a38abb7fde2bacf1d53
Alexey Smolik <alexey.smolik@mediapark.com>
2022-01-14 11:42:57 +0300

    Added refactoring in routing

--- index.js
+++ index.js
@@ -7,26 +7,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/dist/pages/login/login.html');
 });
 
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/dist/pages/login/login.html');
-});
-
-app.get('/registration', (req, res) => {
-    res.sendFile(__dirname + '/dist/pages/registration/registration.html');
-});
-
-app.get('/error', (req, res) => {
-    res.sendFile(__dirname + '/dist/pages/server-error/server-error.html');
-});
-
-app.get('/profile', (req, res) => {
-    res.sendFile(__dirname + '/dist/pages/profile/profile.html');
-});
-
-app.get('/chat', (req, res) => {
-    res.sendFile(__dirname + '/dist/pages/chat/chat.html');
-});
-
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/dist/pages/client-error/client-error.html');
 });
