2356a943c1f612cf9f4ff75121c509c5a1eb888a
Alexey Smolik <alexey.smolik@mediapark.com>
2022-02-13 12:31:03 +0300

    Initial commit

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 
 app.use(express.static('dist'));
 
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/dist/index.html');
-// });
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/dist/index.html');
+});
 
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/dist/index.html');
